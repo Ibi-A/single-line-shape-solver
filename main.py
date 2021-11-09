8e0ec268c7d086e03cf465b96adacf442bbd05d7
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-09 17:41:13 +0100

    created main structure for the code

--- main.py
+++ main.py
@@ -1,33 +1,63 @@
-class Edge:
-    def __init__(self, vertex_1: int, vertex_2: int) -> None:
-        self.vertex_1 = vertex_1
-        self.vertex_2 = vertex_2
+class OrientedEdge:
+    def __init__(self, origin: int, destination: int) -> None:
+        self.origin = origin
+        self.destination = destination
+
+    def __str__(self) -> str:
+        return f"({self.origin};{self.destination})"
 
 
 class Graph:
-    def __init__(self, edges: list) -> None:
-        self._edges = edges
-
     def __init__(
-        self, edges: list, origin_vertex: int, destination_vertex: int
+        self,
+        remaining_edges: list,
+        used_edges: list = None,
+        origin_vertex: int = None,
+        destination_vertex: int = None,
     ) -> None:
-        self._edges = edges
-        self._use_edge(origin_vertex, destination_vertex)
+        self.remaining_edges = []
+        self.used_edges = []
 
-    def get_edge_by_vertices(self, origin_vertex: int, destination_vertex: int) -> Edge:
-        found_edge = None
+        for remaining_edge in remaining_edges:
+            self.append_available_edge(remaining_edge)
 
-        for edge in self._edges:
-            if (
-                edge.vertex_1 == origin_vertex and edge.vertex_2 == destination_vertex
-            ) or (
-                edge.vertex_1 == destination_vertex and edge.vertex_2 == origin_vertex
-            ):
-                found_edge = edge
-                break
+        if origin_vertex is not None and destination_vertex is not None:
+            self.used_edges = used_edges
+            self.use_edge(origin_vertex, destination_vertex)
 
-        return found_edge
+    def append_available_edge(self, edge: OrientedEdge) -> None:
+        if self.get_edge_in_remaining_edges(edge.origin, edge.destination) is None:
+            self.remaining_edges.append(edge)
 
-    def _use_edge(self, origin_vertex: int, destination_vertex: int):
-        used_edge = self.get_edge_by_vertices(origin_vertex, destination_vertex)
-        self._edges.remove(used_edge)
+        if self.get_edge_in_remaining_edges(edge.destination, edge.origin) is None:
+            self.remaining_edges.append(OrientedEdge(edge.destination, edge.origin))
+
+    def remove_available_edge(self, edge: OrientedEdge) -> None:
+        self.remaining_edges.remove(
+            self.get_edge_in_remaining_edges(edge.origin, edge.destination)
+        )
+        self.remaining_edges.remove(
+            self.get_edge_in_remaining_edges(edge.destination, edge.origin)
+        )
+
+    def get_edge_in_remaining_edges(
+        self, origin: int, destination: int
+    ) -> OrientedEdge:
+        for edge in self.remaining_edges:
+            if edge.origin == origin and edge.destination == destination:
+                return edge
+
+    def use_edge(self, origin_vertex: int, destination_vertex: int) -> None:
+        self.remove_available_edge(
+            self.get_edge_in_remaining_edges(origin_vertex, destination_vertex)
+        )
+        self.used_edges.append(OrientedEdge(origin_vertex, destination_vertex))
+
+    def get_available_destinations_from_vertex(self, origin_vertex: int) -> list:
+        available_destinations = []
+
+        for edge in self.remaining_edges:
+            if edge.origin == origin_vertex:
+                available_destinations.append(edge.destination)
+
+        return available_destinations
